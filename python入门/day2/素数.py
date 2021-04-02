8681b21a0ced2b4fc504296e8f6d77900293f3b0
luhan <765478939@qq.com>
2021-04-02 13:54:00 +0800

    求素数的第三种写法：使用for else来实现

--- python入门/day2/素数.py
+++ python入门/day2/素数.py
@@ -18,3 +18,13 @@ for i in range(2, 100 + 1):
             break
     if isPrimeNum:
         print(i, "是素数")
+
+print("======================================================")
+
+# 第三种写法：for else
+for i in range(2, 100 + 1):
+    for j in range(2, i):
+        if (i % j) == 0:  # 就表明不是素数
+            break
+    else:  # 当for循环没有被打断时(没有执行break、exit等打断循环的代码)，就会执行else的语句块
+        print(i, "是素数")
