db4ab062ffc787b8db9d7aa4526f5c578657e4d5
luhan <765478939@qq.com>
2021-04-14 14:23:08 +0800

    异常处理机制

--- python入门/杨树娟的Python入门到精通课程/异常.py
+++ python入门/杨树娟的Python入门到精通课程/异常.py
@@ -12,4 +12,15 @@ if age >= 18:
 2. 索引越界错误:IndexError
     list=[11,22,33,44]
     item=list[4] # 这里就会抛出索引越界异常,因为所以是从0开始的,而这个list只有4个元素,所以最大索引只能是3
+3. 异常处理机制:其中BaseException是最大的异常类,相当于Java中的Exception
+    语法:
+        try:
+            代码块
+        except 异常类型:
+            异常处理代码块
+        except 异常类型2:
+            异常处理代码块2
+    打印异常信息:
+        import traceback
+        traceback.print_exc() # 打印异常信息
 '''
