4054864c19a050ec0132a77374eab79c164b55f1
luhan <765478939@qq.com>
2021-04-16 09:56:06 +0800

    添加学生信息

--- python入门/杨树娟的Python入门到精通课程/学生信息管理系统/stusystem.py
+++ python入门/杨树娟的Python入门到精通课程/学生信息管理系统/stusystem.py
@@ -11,9 +11,59 @@ menuStr = '''
 \t\t\t\t\t\t\t\t\t0. 退出系统
 '''
 
+# 保存学生信息的文件名称
+file_name = "students.txt"
+
 
 # 新增学生信息
+def save(lst):
+    with open(file_name, "a", encoding="utf-8"):
+        with open(file_name, "w", encoding="utf-8") as file:
+            for stu_info in lst:
+                file.write(str(stu_info) + '\n')
+
+
 def insert():
+    student_list = []
+    while True:
+        stu_id = input("请输入学生编号:")
+        stu_name = input("请输入学生姓名:")
+        english_grade = python_grade = java_grade = 0
+
+        # 处理英语成绩
+        while True:
+            try:
+                english_grade = int(input("请输入英语成绩:"))
+                break
+            except:
+                print("成绩输入错误,请重新输入...ps:请输入数字")
+                continue
+        # 处理python成绩
+        while True:
+            try:
+                python_grade = int(input("请输入Python成绩:"))
+                break
+            except:
+                print("成绩输入错误,请重新输入...ps:请输入数字")
+                continue
+        # 处理Java成绩
+        while True:
+            try:
+                java_grade = int(input("请输入Java成绩:"))
+                break
+            except:
+                print("成绩输入错误,请重新输入...ps:请输入数字")
+                continue
+
+        # 信息都输入完毕后,将信息保存到字典中,然后将字典保存进列表中,带添加完毕后将列表写到文件中
+        stu_info = {"id": stu_id, "stu_name": stu_name, "english_grade": english_grade, "python_grade": python_grade,
+                    "java_grade": java_grade}
+        student_list.append(stu_info)
+        answer = input("是否继续输入学生信息?y/n:")
+        if answer == 'n':
+            break
+    # 输入完毕后将学生信息进保存到文件中
+    save(student_list)
     pass
 
 
@@ -57,9 +107,9 @@ def main():
             continue
         if choose in range(8):
             if choose == 0:
-                answer = input("您确定要退出吗?y/n")
+                answer = input("您确定要退出吗?y/n:")
                 if answer == 'y':
-                    print("感谢您的使用!!!")
+                    print("感谢您使用学生信息管理系统!!!")
                     break
                 else:
                     continue
